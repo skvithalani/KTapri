86edd7b5ce33e34391573a2e10530d8d906cac5d
Saloni Vithalani <salonivithalani@INsaloni.local>
2016-03-14 15:54:18 +0530

    Tests added for subject method

--- client/EmailUtilities.js
+++ client/EmailUtilities.js
@@ -1,20 +1,31 @@
-var isDefined = function (content, fullName) {
-    return Ktapri.stripToNull(content) && Ktapri.stripToNull(fullName);
-};
+var isDefined = function (argsArray) {
+    var result = true;
+    argsArray.forEach(function (arg) {
+        if(!Ktapri.stripToNull(arg)) {
+            result = false;
+            return;
+        }
+    });
 
+    return result;
+};
 var isPurposeAddNote = function (purpose) {
     return Ktapri.purposes.ADDNOTE == purpose;
-};
-
-var subjectToAddNote = function(properties) {
-    return properties.firstName + " wants to " + properties.type + " - " + properties.title;
 };
 
 var EOM = function (contentValue) {
     return (!contentValue ? " <EOM>" : "");
 };
 
+var subjectToAddNote = function(properties) {
+    if(!isDefined([properties.firstName, properties.type, properties.title]))
+        return null;
+    return properties.firstName + " wants to " + properties.type + " - " + properties.title;
+};
+
 var subjectToAddResponse = function (properties, contentValue) {
+    if(!isDefined([properties.firstName, properties.title]))
+        return null;
     return properties.firstName + " has responded to your note - " + properties.title + EOM(contentValue);
 };
 
@@ -24,12 +35,15 @@ Ktapri.purposes = {
 };
 
 Ktapri.content = function(fullName, content) {
-    if(isDefined(content, fullName))
+    if(isDefined([fullName, content]))
         return "<p><strong>" + fullName + "</strong> says " + content + "</p>";
     return null;
 };
 
 Ktapri.subject = function(properties, contentValue) {
+
+    if(!properties || !properties.purpose) return null;
+
     if (isPurposeAddNote(properties.purpose))
         return subjectToAddNote(properties);
     return subjectToAddResponse(properties, contentValue);
@@ -37,8 +51,9 @@ Ktapri.subject = function(properties, contentValue) {
 
 Ktapri.prepareAndSendEmail = function (properties) {
     var contentValue = Ktapri.content(properties.fullName, properties.content);
+    var subject = Ktapri.subject(properties, contentValue);
     Meteor.call('sendEmail',
                 properties.to,
-                Ktapri.subject(properties, contentValue),
+                subject,
                 contentValue);
 };
--- tests/jasmine/client/unit/SpecEmailUtilities.js
+++ tests/jasmine/client/unit/SpecEmailUtilities.js
@@ -37,6 +37,78 @@ describe("Email Utilities", function () {
     });
 
     describe("subject", function () {
-       it("")
+        it("should be null when properties passed is null",function(){
+            expect(Ktapri.subject()).toBeNull();
+        });
+
+        it("should be null when purpose is passed null", function(){
+           expect(Ktapri.subject({
+               "purpose" : null
+           })).toBeNull();
+        });
+
+        it("should be null when purpose is passed null", function(){
+            expect(Ktapri.subject({
+                "purpose" : null
+            })).toBeNull();
+        });
+
+        it("should be null when firstName is passed null", function(){
+            expect(Ktapri.subject({
+                "purpose" : Ktapri.purposes.ADDNOTE,
+                "firstName": null
+            })).toBeNull();
+        });
+
+        it("should be null when firstName, type or title is passed null", function(){
+            expect(Ktapri.subject({
+                "purpose" : Ktapri.purposes.ADDNOTE,
+                "firstName": null,
+                "type": null,
+                "title" : null
+            })).toBeNull();
+        });
+
+        it("should be null when firstName or title is passed null and purpose is ADDRESPONSE", function(){
+            expect(Ktapri.subject({
+                "purpose" : Ktapri.purposes.ADDRESPONSE,
+                "firstName": null,
+                "title" : null
+            })).toBeNull();
+        });
+
+        it("should be 'Saloni wants to learn - Spanish' when purpose is ADDRESPONSE, type is learn, firstName is Saloni and title is Spanish", function(){
+            expect(Ktapri.subject({
+                "purpose" : Ktapri.purposes.ADDNOTE,
+                "firstName": "Saloni",
+                "title" : "Spanish",
+                "type" : "learn"
+            })).toBe("Saloni wants to learn - Spanish");
+        });
+
+        it("should be 'Saloni wants to share - recipe' when purpose is ADDRESPONSE, type is share, firstName is Saloni and title is recipe", function(){
+            expect(Ktapri.subject({
+                "purpose" : Ktapri.purposes.ADDNOTE,
+                "firstName": "Saloni",
+                "title" : "recipe",
+                "type" : "share"
+            })).toBe("Saloni wants to share - recipe");
+        });
+
+        it("should be 'Saloni has reponded to your note - recipe <EOM>' when firstName is Saloni, title is recipe, purpose is ADDRESPONSE and contentValue as null", function(){
+            expect(Ktapri.subject({
+                "purpose" : Ktapri.purposes.ADDRESPONSE,
+                "firstName": "Saloni",
+                "title" : "recipe",
+            })).toBe("Saloni has responded to your note - recipe <EOM>");
+        });
+
+        it("should be 'Saloni has reponded to your note - recipe' when firstName is Saloni, title is recipe, purpose is ADDRESPONSE and contentValue as content", function(){
+            expect(Ktapri.subject({
+                "purpose" : Ktapri.purposes.ADDRESPONSE,
+                "firstName": "Saloni",
+                "title" : "recipe",
+            }, "content")).toBe("Saloni has responded to your note - recipe");
+        });
     });
 });

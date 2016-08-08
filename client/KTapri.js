6129935fcb137eb166ec04a5a4e1ff36150aca33
Saloni Vithalani <salonivithalani@INsaloni.local>
2016-08-08 23:40:12 +0530

    Login issue

--- client/KTapri.js
+++ client/KTapri.js
@@ -8,38 +8,54 @@ if (Meteor.isClient) {
 
         Ktapri.init(this, $meteor);
 
+        this.validateUser = function() {
+            var that = this;
+            if (!(Meteor.user() && that.userId)) {
+                if(!that.isRedirectedTohome){
+                    that.isRedirectedTohome = true;
+                    window.location.reload("http://localhost:3000");
+                }
+            }
+            else{
+                that.isRedirectedTohome = false;
+            }
+        };
+
         this.submitNote = function () {
-            Meteor.call("submitNote", {
-                title: this.titleToAdd,
-                content: this.contentToAdd,
-                name: this.user.name,
-                ownerId: this.userId,
-                ownerEmail: this.user.email,
-                type: this.type,
-                createdAt: new Date(),
-                updatedAt: new Date(),
-                responses: new Array(),
-            });
+                this.validateUser();
+                Meteor.call("submitNote", {
+                    title: this.titleToAdd,
+                    content: this.contentToAdd,
+                    name: this.user.name,
+                    ownerId: this.userId,
+                    ownerEmail: this.user.email,
+                    type: this.type,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    responses: new Array(),
+                });
 
-            Ktapri.prepareAndSendEmail({
-                "firstName": this.user.given_name,
-                "title": this.titleToAdd,
-                "fullName": this.user.name,
-                "content": this.contentToAdd,
-                "to": this.user.email,
-                "purpose": Ktapri.purposes.ADDNOTE,
-                "type": this.type
-            });
+                Ktapri.prepareAndSendEmail({
+                    "firstName": this.user.given_name,
+                    "title": this.titleToAdd,
+                    "fullName": this.user.name,
+                    "content": this.contentToAdd,
+                    "to": this.user.email,
+                    "purpose": Ktapri.purposes.ADDNOTE,
+                    "type": this.type
+                });
 
-            $('#addModal').modal('hide');
+                $('#addModal').modal('hide');
         };
 
         this.resetForm = function () {
+            this.validateUser();
             this.titleToAdd = '';
             this.contentToAdd = '';
         };
 
         this.editNote = function (note) {
+            this.validateUser();
             if (!this.fromResponses && Meteor.user() && this.userId && this.userId === note.ownerId) {
                 this.titleToEdit = note.title;
                 this.contentToEdit = note.content;
@@ -50,6 +66,7 @@ if (Meteor.isClient) {
         };
 
         this.saveNote = function () {
+            this.validateUser();
             Meteor.call("updateNote",
                         {_id: this.noteId},
                         {
@@ -60,19 +77,20 @@ if (Meteor.isClient) {
         };
 
         this.deleteNote = function () {
+            this.validateUser();
             Meteor.call("removeNote", {_id: this.noteId});
         };
 
         this.openAddResponseModal = function (note) {
-            if (Meteor.user() && this.userId) {
-                this.responseToAdd = '';
-                this.note = note;
-                $('#addResponseModal').modal('show');
-                this.fromResponses = true;
-            }
+            this.validateUser();
+            this.responseToAdd = '';
+            this.note = note;
+            $('#addResponseModal').modal('show');
+            this.fromResponses = true;
         };
 
         this.submitResponse = function (note) {
+            this.validateUser();
             note = note || this.note;
             if (note.responses == null) {
                 note.responses = new Array();
@@ -98,6 +116,7 @@ if (Meteor.isClient) {
         };
 
         this.editResponse = function (currentResponse) {
+            this.validateUser();
             if (Meteor.user() && this.userId && this.userId === currentResponse.ownerId) {
                 $('#Text_' + currentResponse._id).text(currentResponse.response);
                 $('#Response_' + currentResponse._id).addClass('hide');
@@ -108,12 +127,14 @@ if (Meteor.isClient) {
         };
 
         this.saveResponse = function (currentResponse) {
+            this.validateUser();
             currentResponse.response = $('#Text_' + currentResponse._id).val();
             currentResponse.updatedAt = new Date();
             displayResponses(currentResponse);
         };
 
         this.deleteResponse = function (note, currentResponse) {
+            this.validateUser();
             if (Meteor.user() && this.userId && this.userId === currentResponse.ownerId) {
                 var $indexOf = note.responses.indexOf(currentResponse);
                 note.responses.splice($indexOf, 1);
@@ -121,6 +142,7 @@ if (Meteor.isClient) {
         };
 
         var displayResponses = function (currentResponse) {
+            this.validateUser();
             $('#Response_' + currentResponse._id).removeClass('hide');
             $('#Edit_' + currentResponse._id).addClass('hide');
             $('#Save_' + currentResponse._id).addClass('hide');
@@ -128,19 +150,19 @@ if (Meteor.isClient) {
         };
 
         this.seeResponses = function (note) {
-            if(Meteor.user()){
-                var noteId = note._id
-                this.expand = false;
-                this.responseToAdd = '';
-                for(i = 0; i < note.responses.length; i++){
-                    displayResponses(note.responses[i]);
-                }
-                $('#response_for_' + noteId).modal('show');
-                this.fromResponses = true;
+            this.validateUser();
+            var noteId = note._id;
+            this.expand = false;
+            this.responseToAdd = '';
+            for(i = 0; i < note.responses.length; i++){
+                displayResponses(note.responses[i]);
             }
+            $('#response_for_' + noteId).modal('show');
+            this.fromResponses = true;
         };
 
         this.addResponse = function () {
+            this.validateUser();
             this.responseToAdd = '';
             this.expand = true
         };
